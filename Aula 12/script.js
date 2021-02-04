677b99358796b78b742df77946379aa2c9047eb1
lokocural <lucascuranmartins@gmail.com>
2021-02-04 10:14:34 -0300

    Atualizações exercicios
    Atualizações exercicios

--- Aula 12/script.js
+++ Aula 12/script.js
@@ -1,1 +1,19 @@
-alert('Olá')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var msg = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA
+        img.src = 'fotomanha.jpg'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18) {
+        //BOA TARDE
+        img.src = "fototarde.jpg"
+    } else {
+        //BOA NOITE
+        img.src = "fotonoite.jpg"
+    }
+
+}
